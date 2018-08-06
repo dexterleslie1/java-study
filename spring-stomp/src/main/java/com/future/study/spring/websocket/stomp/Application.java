78eebc357f2d6e33146b567f0554a14286aa3e21
dexterleslie@gmail.com <dexterleslie@gmail.com>
2018-08-06 14:59:39 +0800

    spring stomp学习

---
+++ spring-stomp/src/main/java/com/future/study/spring/websocket/stomp/Application.java
@@ -0,0 +1,21 @@
+package com.future.study.spring.websocket.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author Dexterleslie
+ * @date 2018年08月06日
+ * @time 12:20
+ */
+@SpringBootApplication
+public class Application {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String []args){
+        SpringApplication.run(Application.class,args);
+    }
+}
