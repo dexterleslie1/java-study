4fe8c1b374121451597c0580c7bb5823966d77a5
dexterleslie@gmail.com <dexterleslie@gmail.com>
2018-10-08 00:37:07 +0800

    新增bootstrap项目

---
+++ bootstrap/src/main/java/com/future/study/bootstrap/Application.java
@@ -0,0 +1,18 @@
+package com.future.study.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dexterleslie.Chan
+ */
+@SpringBootApplication
+public class Application {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String []args){
+        SpringApplication.run(Application.class, args);
+    }
+}
