30ed91cbd5115844623c027ebc67715564011550
a@gmail.com <a@gmail.com>
2018-10-31 10:40:10 +0800

    spring-security-web禁用csrf检测

--- spring-security-web/src/main/java/com/future/study/spring/security/web/WebSecurityConfig.java
+++ spring-security-web/src/main/java/com/future/study/spring/security/web/WebSecurityConfig.java
@@ -8,24 +8,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
- * @author Dexterleslie
- * @date 2018年08月14日
- * @time 9:14
+ * @author Dexterleslie.Chan
  */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/api/auth/login").permitAll()
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/api/auth/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new LoginFilter(new AntPathRequestMatcher("/api/auth/login")), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new AuthFilter(),UsernamePasswordAuthenticationFilter.class);
     }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
-//        builder.userDetailsService(new MyUserDetailsService()).passwordEncoder(new BCryptPasswordEncoder());
-//    }
 }
