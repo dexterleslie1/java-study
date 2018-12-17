8c826f93aace7ba2c42b9319a82aabccad41fbd7
a@gmail.com <a@gmail.com>
2018-12-17 15:55:53 +0800

    large-scale-solution项目UserRepositoryJDBCTest新增testInsertByUsingJDBCShardDatabases测试方法

--- large-scale-solution/src/test/java/com/future/study/spring/large/scale/solution/UserRepositoryJDBCTest.java
+++ large-scale-solution/src/test/java/com/future/study/spring/large/scale/solution/UserRepositoryJDBCTest.java
@@ -35,7 +35,7 @@ public class UserRepositoryJDBCTest {
 
 		Date date1 = new Date();
 		ExecutorService executorService = Executors.newCachedThreadPool();
-		for(int j = 0; j < 1000 ; j++) {
+		for(int j = 0; j < 100 ; j++) {
 			final int j1 = j;
 			executorService.submit(new Runnable() {
 				@Override
@@ -51,7 +51,7 @@ public class UserRepositoryJDBCTest {
 						conn = (Connection) DriverManager.getConnection(url, username, password);
 
 						Random random = new Random();
-						int batchSize = 1000;
+						int batchSize = 10000;
 						int size = sex.size();
 						int start = j1 * batchSize + 1;
 						int end = start + batchSize;
@@ -118,7 +118,7 @@ public class UserRepositoryJDBCTest {
 
 		Date date1 = new Date();
 		ExecutorService executorService = Executors.newCachedThreadPool();
-		for(int j = 0; j < 1000 ; j++) {
+		for(int j = 0; j < 100 ; j++) {
 			final int j1 = j;
 			executorService.submit(new Runnable() {
 				@Override
@@ -136,7 +136,95 @@ public class UserRepositoryJDBCTest {
 						conn = (Connection) DriverManager.getConnection(url, username, password);
 
 						Random random = new Random();
-						int batchSize = 1000;
+						int batchSize = 10000;
+						int size = sex.size();
+						int start = j1 * batchSize + 1;
+						int end = start + batchSize;
+						for (int i = start; i < end; i++) {
+							preparedStatement = (PreparedStatement) conn.prepareStatement(sql);
+							preparedStatement.setInt(1,i);
+							preparedStatement.setDate(2, new java.sql.Date(new Date().getTime()));
+							preparedStatement.setString(3, "ln" + RandomUtils.getCharacterAndNumber(20));
+							preparedStatement.setString(4, "nn" + RandomUtils.getCharacterAndNumber(20));
+							preparedStatement.setString(5,  RandomUtils.getCharacterAndNumber(20));
+							preparedStatement.setString(6, "135" + RandomUtils.getCharacterAndNumber(20));
+							preparedStatement.setString(7, "gm" + RandomUtils.getCharacterAndNumber(20) + "@gmail.com");
+							int index = random.nextInt(size);
+							preparedStatement.setInt(8, sex.get(index));
+							preparedStatement.executeUpdate();
+							preparedStatement.close();
+							preparedStatement = null;
+						}
+					}catch(Exception ex){
+						ex.printStackTrace();
+					}finally {
+						if(preparedStatement != null){
+							try {
+								preparedStatement.close();
+								preparedStatement = null;
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+						}
+						if(conn != null){
+							try {
+								conn.close();
+								conn = null;
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			});
+		}
+
+		executorService.shutdown();
+		while(!executorService.awaitTermination(1, TimeUnit.SECONDS));
+
+		Date date2 = new Date();
+		long milliseconds = date2.getTime() - date1.getTime();
+		logger.debug("耗时："+ milliseconds + "毫秒");
+	}
+
+	@Test
+	public void testInsertByUsingJDBCShardDatabases() throws InterruptedException {
+		List<String> urls = new ArrayList<String>();
+		urls.add("jdbc:mysql://192.168.1.153:3306/backend");
+		urls.add("jdbc:mysql://192.168.1.154:3306/backend");
+		urls.add("jdbc:mysql://192.168.1.155:3306/backend");
+		urls.add("jdbc:mysql://192.168.1.156:3306/backend");
+
+		int databaseCount = urls.size();
+
+		String sql = "insert into t_user(id,createTime,loginname,nickname,password,phone,email,sex)" +
+				" values(?,?,?,?,?,?,?,?)";
+
+		List<Integer> sex = new ArrayList<Integer>();
+		sex.add(0);
+		sex.add(1);
+		sex.add(2);
+
+		Date date1 = new Date();
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		for(int j = 0; j < 200 ; j++) {
+			final int j1 = j;
+			executorService.submit(new Runnable() {
+				@Override
+				public void run() {
+					Connection conn = null;
+					PreparedStatement preparedStatement = null;
+					try {
+						String driver = "com.mysql.jdbc.Driver";
+						int index1 = j1 % databaseCount;
+						String url = urls.get(index1);
+						String username = "root";
+						String password = "Root@123";
+						Class.forName(driver); //classLoader,加载对应驱动
+						conn = (Connection) DriverManager.getConnection(url, username, password);
+
+						Random random = new Random();
+						int batchSize = 5000;
 						int size = sex.size();
 						int start = j1 * batchSize + 1;
 						int end = start + batchSize;
