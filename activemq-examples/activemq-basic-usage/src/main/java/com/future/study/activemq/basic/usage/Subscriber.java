4ee88914621eb385539f924211900e7741e26e76
dexterleslie@gmail.com <dexterleslie@gmail.com>
2019-04-17 21:41:47 +0800

    ActiveMQ demonstrating multiple subscribers.

--- activemq-examples/activemq-basic-usage/src/main/java/com/future/study/activemq/basic/usage/Subscriber.java
+++ activemq-examples/activemq-basic-usage/src/main/java/com/future/study/activemq/basic/usage/Subscriber.java
@@ -1,7 +1,66 @@
 package com.future.study.activemq.basic.usage;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 /**
  * @author Dexterleslie.Chan
  */
 public class Subscriber {
+    private String clientId = null;
+    private String topic = null;
+    private Connection connection = null;
+    private MessageConsumer messageConsumer = null;
+
+    /**
+     *
+     * @param clientId
+     * @param topic
+     */
+    public Subscriber(String clientId, String topic){
+        this.clientId = clientId;
+        this.topic = topic;
+    }
+
+    /**
+     *
+     * @param timeoutInMilliseconds
+     * @return
+     * @throws JMSException
+     */
+    public String receiveMessage(int timeoutInMilliseconds) throws JMSException {
+        Message message = messageConsumer.receive(timeoutInMilliseconds);
+        if(message != null){
+            TextMessage textMessage = (TextMessage)message;
+            return textMessage.getText();
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @throws JMSException
+     */
+    public void connect() throws JMSException {
+        ConnectionFactory connectionFactory =
+                new ActiveMQConnectionFactory(Config.BrokerUrl);
+        connection = connectionFactory.createConnection();
+        connection.setClientID(clientId);
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Topic topic = session.createTopic(this.topic);
+        messageConsumer = session.createConsumer(topic);
+        // Start the connection in order to receive messages
+        connection.start();
+    }
+
+    /**
+     *
+     * @throws JMSException
+     */
+    public void close() throws JMSException {
+        if(this.connection != null){
+            this.connection.close();
+        }
+    }
 }
--- activemq-examples/activemq-basic-usage/src/test/java/com/future/study/activemq/basic/usage/MultipleSubscriberExampleTest.java
+++ activemq-examples/activemq-basic-usage/src/test/java/com/future/study/activemq/basic/usage/MultipleSubscriberExampleTest.java
@@ -1,6 +1,5 @@
 package com.future.study.activemq.basic.usage;
 
-import org.apache.activemq.util.Suspendable;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,37 +9,45 @@ import javax.jms.JMSException;
 import java.util.Date;
 
 /**
- * Examples for demonstrating basic usage of activemq java client
+ * Examples for demonstrating multiple subscriber
  * Reference sites:
  * https://codenotfound.com/jms-publish-subscribe-messaging-example-activemq-maven.html
  * @author Dexterleslie.Chan
  */
-public class BasicUsageExampleTest {
+public class MultipleSubscriberExampleTest {
     @Test
-    public void test_publish_subscribe() throws JMSException {
+    public void test_publish_multiple_subscribe() throws JMSException {
         String stringTimeNow = (new Date()).toString();
         publisher.sendMessage(stringTimeNow);
 
-        String message = subscriber.receiveMessage(1000);
+        String message = subscriberOne.receiveMessage(1000);
+        Assert.assertEquals(stringTimeNow, message);
+
+        message = subscriberTwo.receiveMessage(1000);
         Assert.assertEquals(stringTimeNow, message);
     }
 
     private Publisher publisher = null;
-    private Subscriber subscriber = null;
+    private Subscriber subscriberOne = null;
+    private Subscriber subscriberTwo = null;
 
     @Before
     public void setup() throws JMSException {
-        String topic = "topic-publish-subscribe";
+        String topic = "topic-publish-multiple-subscribe";
         publisher = new Publisher("publisher-one", topic);
         publisher.connect();
 
-        subscriber = new Subscriber("subscriber-one", topic);
-        subscriber.connect();
+        subscriberOne = new Subscriber("subscriber-one", topic);
+        subscriberOne.connect();
+
+        subscriberTwo = new Subscriber("subscriber-two", topic);
+        subscriberTwo.connect();
     }
 
     @After
     public void teardown() throws JMSException {
         publisher.close();
-        subscriber.close();
+        subscriberOne.close();
+        subscriberTwo.close();
     }
 }
