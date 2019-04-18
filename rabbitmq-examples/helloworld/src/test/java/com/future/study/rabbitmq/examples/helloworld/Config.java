c2060a6115358892c763fd6b428de6aa7728360f
dexterleslie@gmail.com <dexterleslie@gmail.com>
2019-04-19 01:14:37 +0800

    rabbitmq helloworld tutorial for demonstrating basic usage rabbitmq java client

---
+++ rabbitmq-examples/helloworld/src/test/java/com/future/study/rabbitmq/examples/helloworld/Config.java
@@ -0,0 +1,19 @@
+package com.future.study.rabbitmq.examples.helloworld;
+
+/**
+ * @author Dexterleslie.Chan
+ */
+public class Config {
+    /**
+     * Host ip address for rabbitmq server
+     */
+    final static String RabbitMQServerHost = "192.168.102.128";
+    /**
+     * Username for login rabbitmq server
+     */
+    final static String RabbitMQUsername = "root";
+    /**
+     * User password for login rabbitmq server
+     */
+    final static String RabbitMQPassword = "123456";
+}
---
+++ rabbitmq-examples/helloworld/src/test/java/com/future/study/rabbitmq/examples/helloworld/HelloWorldExampleTest.java
@@ -0,0 +1,52 @@
+package com.future.study.rabbitmq.examples.helloworld;
+
+import com.rabbitmq.client.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * This test case is demonstrating for rabbitmq java client connect to rabbitmq server
+ * @author Dexterleslie.Chan
+ */
+public class HelloWorldExampleTest {
+    private String messageConsume = null;
+    @Test
+    public void test_tutorial_helloworld() throws IOException, TimeoutException, InterruptedException {
+        String queueName = "rabbitmq-examples-tutorial-helloworld";
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(Config.RabbitMQServerHost);
+        connectionFactory.setUsername(Config.RabbitMQUsername);
+        connectionFactory.setPassword(Config.RabbitMQPassword);
+
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(queueName, false, false, false, null);
+
+        String messageToPublish = "Hello World!";
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        DeliverCallback deliverCallback = new DeliverCallback() {
+            @Override
+            public void handle(String consumerTag, Delivery delivery) throws IOException {
+                messageConsume = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                countDownLatch.countDown();
+            }
+        };
+        // Ready to consume rabbitmq message
+        channel.basicConsume(queueName, deliverCallback, consumerTag -> {});
+
+        channel.basicPublish("", queueName, null, messageToPublish.getBytes(StandardCharsets.UTF_8));
+
+        countDownLatch.await(2000, TimeUnit.MILLISECONDS);
+
+        connection.close();
+        Assert.assertEquals(messageToPublish, messageConsume);
+    }
+}
