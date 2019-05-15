b0f5807c646559ec0fa3d7388eecc994ef8650b3
Dexter.Chan <wwwwu8899@vip.qq.com>
2019-05-15 22:30:06 +0800

    改进redisson-lock-demo

--- redisson/redisson-lock/src/test/java/com/xy/demo/redisson/lock/RedissonLockPerformanceTests.java
+++ redisson/redisson-lock/src/test/java/com/xy/demo/redisson/lock/RedissonLockPerformanceTests.java
@@ -1,5 +1,6 @@
 package com.xy.demo.redisson.lock;
 
+import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 
+import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
@@ -15,6 +17,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class RedissonLockPerformanceTests {
+    private final static Logger logger = Logger.getLogger(RedissonLockPerformanceTests.class);
+
     private String redisHost = "192.168.1.229";
     private int redisPort = 6379;
     private String redisPassword = "123456";
@@ -24,6 +28,7 @@ public class RedissonLockPerformanceTests {
 
     @Test
     public void test1() throws InterruptedException {
+        Date timeStart = new Date();
         ExecutorService executorService = Executors.newCachedThreadPool();
         for(int j=0; j<200; j++) {
             executorService.submit(new Runnable() {
@@ -53,8 +58,12 @@ public class RedissonLockPerformanceTests {
                 }
             });
         }
+
         executorService.shutdown();
         while(!executorService.awaitTermination(100, TimeUnit.MILLISECONDS));
+        Date timeEnd = new Date();
+        long milliseconds = timeEnd.getTime() - timeStart.getTime();
+        logger.info("耗时" + milliseconds + "毫秒");
     }
 
     @Before
