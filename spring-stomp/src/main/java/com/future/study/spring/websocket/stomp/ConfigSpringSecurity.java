adaac4ff550e68294225378117e16d3df5c90b3d
Dexter.Chan <wwwwu8899@vip.qq.com>
2019-05-28 21:11:04 +0800

    spring-stomp spring-security 忽略所有url拦截

--- spring-stomp/src/main/java/com/future/study/spring/websocket/stomp/ConfigSpringSecurity.java
+++ spring-stomp/src/main/java/com/future/study/spring/websocket/stomp/ConfigSpringSecurity.java
@@ -2,8 +2,14 @@ package com.future.study.spring.websocket.stomp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.LinkedList;
 
 /**
  *
@@ -11,6 +17,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class ConfigSpringSecurity extends WebSecurityConfigurerAdapter {
+    /**
+     *
+     * @param web
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        LinkedList<RequestMatcher> matcherList = new LinkedList<>();
+        matcherList.add(new AntPathRequestMatcher("/**"));
+        web.ignoring().requestMatchers(new OrRequestMatcher(matcherList));
+    }
+
+    /**
+     *
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
