059e3775bc0e9016bd4709b9a60d1b9c7a9598e9
dexterleslie@gmail.com <dexterleslie@gmail.com>
2019-06-03 21:32:39 +0800

    spring-websocket demo 自定义协议

--- spring-websocket/src/main/java/com/future/study/spring/websocket/WebSocketRequestHandler.java
+++ spring-websocket/src/main/java/com/future/study/spring/websocket/WebSocketRequestHandler.java
@@ -1,16 +1,26 @@
 package com.future.study.spring.websocket;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
+import org.springframework.util.StringUtils;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  */
 public class WebSocketRequestHandler extends TextWebSocketHandler {
     private final static Logger logger = Logger.getLogger(WebSocketRequestHandler.class);
+
+    private final static String KeyAction = "action";
+
+    private Map<String, WebSocketSession> mapSessions = new HashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -20,7 +30,45 @@ public class WebSocketRequestHandler extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        logger.debug("Received client message " + message);
+        String payload = message.getPayload();
+        if(!StringUtils.isEmpty(payload)){
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, String> data = null;
+            try {
+                data = objectMapper.readValue(payload, Map.class);
+            }catch(JsonParseException ex){
+                //
+            }
+            if(data != null && data.containsKey(KeyAction)){
+                String action = data.get(KeyAction);
+                if("CONNECT".equals(action)){
+                    String username = data.get("username");
+                    if(!StringUtils.isEmpty(username)){
+                        session.getAttributes().put("username", username);
+                        mapSessions.put(username, session);
+                        logger.debug("用户" + username + " 连接websocket服务器");
+                    }
+                }else if("MESSAGE".equals(action)){
+                    String toUser = data.get("toUser");
+                    if(!StringUtils.isEmpty(toUser)){
+                        WebSocketSession toSession = mapSessions.get(toUser);
+                        String content = data.get("content");
+                        String fromUser = (String)session.getAttributes().get("username");
+                        if(toSession != null) {
+                            Map<String, String> messageMap = new HashMap<>();
+                            messageMap.put("fromUser", fromUser);
+                            messageMap.put("content", content);
+                            toSession.sendMessage(new TextMessage(new ObjectMapper().writeValueAsString(messageMap)));
+                            logger.debug("用户" + fromUser +" 给用户" + toUser + " 发送消息 " + content);
+                        }else {
+                            logger.debug("用户" + fromUser + " 尝试给用户" + toUser + " 发送消息，但不在线");
+                        }
+
+                    }
+                }
+            }
+        }
+
         super.handleTextMessage(session, message);
     }
 
--- spring-websocket/src/test/java/com/future/study/spring/websocket/WebSocketClientTests.java
+++ spring-websocket/src/test/java/com/future/study/spring/websocket/WebSocketClientTests.java
@@ -1,5 +1,6 @@
 package com.future.study.spring.websocket;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.util.concurrent.ListenableFuture;
@@ -11,6 +12,10 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
 import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -31,8 +36,29 @@ public class WebSocketClientTests {
         }, url);
 
         WebSocketSession socketSession = future.get();
-        socketSession.sendMessage(new TextMessage("{\"command\":\"test\",\"parameters\":{\"type\":\"conn\"}}"));
+
+        Map<String, String> requestMap = new HashMap<>();
+        requestMap.put("action", "CONNECT");
+        requestMap.put("username", "A");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(requestMap);
+        socketSession.sendMessage(new TextMessage(json));
+
+        for(int i=0; i<100; i++){
+            Random random = new Random();
+            byte []bytes = new byte[10];
+            random.nextBytes(bytes);
+            String content = Base64.getEncoder().encodeToString(bytes);
+
+            Map<String, String> messageMap = new HashMap<>();
+            messageMap.put("action", "MESSAGE");
+            messageMap.put("toUser", "B");
+            messageMap.put("content", "随机数据 " + content);
+            TextMessage message = new TextMessage(new ObjectMapper().writeValueAsString(messageMap));
+            socketSession.sendMessage(message);
+            Thread.sleep(1000);
+        }
+
         socketSession.close();
-//        Thread.sleep(1000);
     }
 }
