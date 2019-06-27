3699027c1d7402e58475195275dfb9e3b1ff2ca5
Dexter.Chan <wwwwu8899@vip.qq.com>
2019-06-27 16:37:53 +0800

    新增redisson-cluster-lock demo

---
+++ redisson/redisson-cluster-lock/src/test/java/com/xy/demo/redisson/lock/RedissonClusterLockPerformanceTests.java
@@ -0,0 +1,81 @@
+package com.xy.demo.redisson.lock;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class RedissonClusterLockPerformanceTests {
+    private final static Logger logger = Logger.getLogger(RedissonClusterLockPerformanceTests.class);
+
+    private RedissonClient redisson = null;
+    private Random random = new Random();
+
+    @Test
+    public void test1() throws InterruptedException {
+        Date timeStart = new Date();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for(int j=0; j<200; j++) {
+            executorService.submit(new Runnable() {
+                public void run() {
+                    for (int i = 0; i < 1000; i++) {
+                        String key = UUID.randomUUID().toString();
+                        RLock mylock = redisson.getLock(key);
+                        boolean isLocked = false;
+                        try {
+                            isLocked = mylock.tryLock(10, 10000, TimeUnit.MILLISECONDS);
+
+                            int milliseconds = random.nextInt(50);
+                            if(milliseconds ==0){
+                                milliseconds = 2;
+                            }
+                            Thread.sleep(milliseconds);
+                        } catch (InterruptedException e) {
+                            if(isLocked){
+                                mylock = redisson.getLock(key);
+                                if(mylock != null) {
+                                    mylock.unlock();
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        executorService.shutdown();
+        while(!executorService.awaitTermination(100, TimeUnit.MILLISECONDS));
+        Date timeEnd = new Date();
+        long milliseconds = timeEnd.getTime() - timeStart.getTime();
+        logger.info("耗时" + milliseconds + "毫秒");
+    }
+
+    @Before
+    public void setup(){
+        Config config = new Config();
+        config.useClusterServers().addNodeAddress(
+                "redis://192.168.1.111:6381",
+                "redis://192.168.1.111:6380",
+                "redis://192.168.1.111:6379"
+        );
+        redisson = Redisson.create(config);
+    }
+
+    @After
+    public void teardown(){
+        if(redisson != null){
+            redisson.shutdown();
+        }
+    }
+}
