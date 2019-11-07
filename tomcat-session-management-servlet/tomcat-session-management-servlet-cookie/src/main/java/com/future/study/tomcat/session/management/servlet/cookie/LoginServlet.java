02d4973691afc043cd309a421405f4aeb6f3c213
dexterleslie@gmail.com <dexterleslie@gmail.com>
2019-11-07 09:23:53 +0800

    新增 tomcat-session-management-servlet demo

---
+++ tomcat-session-management-servlet/tomcat-session-management-servlet-cookie/src/main/java/com/future/study/tomcat/session/management/servlet/cookie/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.future.study.tomcat.session.management.servlet.cookie;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author dexterleslie@gmail.com
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    private final String userID = "test1";
+    private final String password = "t1";
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+
+        // get request parameters for userID and password
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+
+        if(userID.equals(user) && password.equals(pwd)){
+            Cookie loginCookie = new Cookie("user",user);
+            //setting cookie to expiry in 30 mins
+            loginCookie.setMaxAge(30*60);
+            response.addCookie(loginCookie);
+            response.sendRedirect("LoginSuccess.jsp");
+        }else{
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out= response.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            rd.include(request, response);
+        }
+
+    }
+}
