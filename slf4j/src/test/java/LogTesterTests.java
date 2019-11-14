d9c2f3374b9543c7bb8b006cbfe206c299ce4fba
dexterleslie@gmail.com <dexterleslie@gmail.com>
2019-11-14 18:28:00 +0800

    修改slf4j demo

--- slf4j/src/test/java/LogTesterTests.java
+++ slf4j/src/test/java/LogTesterTests.java
@@ -1,14 +1,26 @@
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Dexterleslie.Chan
  */
 public class LogTesterTests {
+    private final static Logger logger = LoggerFactory.getLogger(LogTesterTests.class);
+
     @Test
     public void test(){
         LogTester logTester=new LogTester();
         logTester.logThisClass();
         logTester.logBusinessError();
         logTester.logUnexpectedException();
+    }
+
+    @Test
+    public void test1() {
+        Exception exception = new Exception("错误1");
+        logger.error(exception.getMessage(), exception);
+        exception = new Exception("错误2");
+        logger.info(exception.getMessage(), exception);
     }
 }
