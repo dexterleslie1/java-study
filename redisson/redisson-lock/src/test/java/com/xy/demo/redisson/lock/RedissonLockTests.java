6b22036e7d3673b7b191ee9cf47a5dcc1c4395f3
dexterleslie@gmail.com <dexterleslie@gmail.com>
2020-01-07 22:38:40 +0800

    修改 redisson/redisson-lock demo 新增测试案列演示 isLocked 函数用法

--- redisson/redisson-lock/src/test/java/com/xy/demo/redisson/lock/RedissonLockTests.java
+++ redisson/redisson-lock/src/test/java/com/xy/demo/redisson/lock/RedissonLockTests.java
@@ -70,6 +70,45 @@ public class RedissonLockTests {
         while(!service.awaitTermination(100, TimeUnit.MILLISECONDS));
     }
 
+    @Test
+    public void testIsLocked() throws InterruptedException {
+        final String key = "fdjiu34938983r399fj3j";
+        ExecutorService service = Executors.newCachedThreadPool();
+        for(int i=0; i<10; i++) {
+            final int seq = i;
+            service.submit(new Runnable() {
+                public void run() {
+                    int milliseconds = Random.nextInt(500);
+                    if(milliseconds<=0) {
+                        milliseconds = 5;
+                    }
+                    try {
+                        Thread.sleep(milliseconds);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    RLock lock = redisson.getLock(key);
+                    try {
+                        lock.tryLock(10, 30000, TimeUnit.MILLISECONDS);
+                        // 锁定2秒
+                        Thread.sleep(2000);
+                        String message = String.format("线程%d 锁获取%s", seq, lock.isLocked()?"成功":"失败");
+                        logger.info(message);
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    } finally {
+                        if (lock!=null && lock.isLocked()) {
+                            lock.unlock();
+                        }
+                    }
+                }
+            });
+        }
+        service.shutdown();
+        while(!service.awaitTermination(100, TimeUnit.MILLISECONDS));
+    }
+
     @Before
     public void setup(){
         Config config = new Config();
