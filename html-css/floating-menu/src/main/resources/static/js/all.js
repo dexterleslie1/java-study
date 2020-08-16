eef6cce8577fbb9aa5c6922708b5b9d8a66d449b
dexterleslie@gmail.com <dexterleslie@gmail.com>
2020-08-16 15:56:18 +0800

    html-css/floating-menu demo新增顶部菜单固定浮动

--- html-css/floating-menu/src/main/resources/static/js/all.js
+++ html-css/floating-menu/src/main/resources/static/js/all.js
@@ -5,14 +5,28 @@ $(document).ready(function(){
 		var elementTemporary = $("<div>div" + i + "</div>");
 		elementDivContent.append(elementTemporary);
 		elementTemporary.height(50);
+		if(i%2==0) {
+			elementTemporary.css("background-color", "#9ff5d0");
+		} else {
+			elementTemporary.css("background-color", "#7acdd6");
+		}
 	}
 	
 	var elementDivFloatingMenu = $("#divFloatingMenu");
 	var floatingMenuOffset = elementDivFloatingMenu.offset();
 	
-	$(window).scroll(function(){
+	$(window).scroll(function() {
 		var scrollTop = $(this).scrollTop();
-		var floatingMenuTop = floatingMenuOffset.top + scrollTop;
-		elementDivFloatingMenu.offset({"top":floatingMenuTop});
+		var top = floatingMenuOffset.top + scrollTop;
+		elementDivFloatingMenu.offset({"top":top});
+	});
+	
+	var elementDivFloatingMenuTop = $("#divFloatingMenuTop");
+	var floatingMenuTopOffset = elementDivFloatingMenuTop.offset();
+	
+	$(window).scroll(function() {
+		var scrollTop = $(this).scrollTop();
+		var top = floatingMenuTopOffset.top + scrollTop;
+		elementDivFloatingMenuTop.offset({"top":top});
 	});
 });
