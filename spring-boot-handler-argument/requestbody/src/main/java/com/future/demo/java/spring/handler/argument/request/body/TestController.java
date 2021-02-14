18a0480e601afac632d99e6cb9f985385d4a50d9
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-02-14 20:46:38 +0800

    改进spring-boot-handler-argument/requestbody demo

--- spring-boot-handler-argument/requestbody/src/main/java/com/future/demo/java/spring/handler/argument/request/body/TestController.java
+++ spring-boot-handler-argument/requestbody/src/main/java/com/future/demo/java/spring/handler/argument/request/body/TestController.java
@@ -3,14 +3,56 @@ package com.future.demo.java.spring.handler.argument.request.body;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author dexterleslie@gmail.com
  */
 @RestController
-@RequestMapping(path = "/api/v1/handler/argument")
+@RequestMapping(path = "/api/v1/handler/argument/request")
 public class TestController {
+    /**
+     * 测试方法使用postman
+     * body数据：body标签页选择 raw 并填写body内容为 {"age":10,"name":"Dexter1","salary":20.1}，contentType选择application/json
+     * @param parameters
+     * @return
+     */
+    @RequestMapping(path = "bodyAsMap")
+    ResponseEntity<String> bodyAsMap(
+            @RequestBody(required = false) Map<String, Object> parameters) {
+        int age = (Integer)parameters.get("age");
+        String name = (String)parameters.get("name");
+        double salary = (Double)parameters.get("salary");
+        System.out.println("age=" + age + ",name=" + name + ",salary=" + salary);
+        Date currentTime = new Date();
+        return ResponseEntity.ok(String.valueOf(currentTime));
+    }
+
+    /**
+     * 测试方法使用postman
+     * body数据：body标签页选择 raw 并填写body内容为 [{"age":10,"name":"Dexter1","salary":20.1},{"age":11,"name":"Dexter2","salary":20.2}]，contentType选择application/json
+     * @param parameters
+     * @return
+     */
+    @RequestMapping(path = "bodyAsListWithEntryMap")
+    ResponseEntity<String> bodyAsListWithEntryMap(
+        @RequestBody(required = false) List<Map<String, Object>> parameters) {
+        StringBuilder builder = new StringBuilder();
+        for(int i=0; i<parameters.size(); i++) {
+            Map<String, Object> parameter = parameters.get(i);
+            int age = (Integer)parameter.get("age");
+            String name = (String)parameter.get("name");
+            double salary = (Double)parameter.get("salary");
+            builder.append("age=" + age + ",name=" + name + ",salary=" + salary);
+            builder.append("\n");
+        }
+        System.out.println(builder.toString());
+        Date currentTime = new Date();
+        return ResponseEntity.ok(String.valueOf(currentTime));
+    }
+
     /**
      * 测试方法使用postman
      * body数据：body标签页选择 raw 并填写body内容，contentType选择application/json
@@ -18,7 +60,7 @@ public class TestController {
      * @param string
      * @return
      */
-    @RequestMapping(path = "request/bodyWithString")
+    @RequestMapping(path = "bodyWithString")
     public ResponseEntity<String> bodyWithString(
             @RequestBody(required = false) String string) {
         String responseString = "bodyString=" + string;
@@ -34,7 +76,7 @@ public class TestController {
      * @param parameter
      * @return
      */
-    @RequestMapping(path = "request/bodyAndParameter")
+    @RequestMapping(path = "bodyAndParameter")
     public ResponseEntity<String> bodyAndParameter(
             @RequestBody(required = false) String string,
             @RequestParam(name = "parameter", required = false) String parameter) {
@@ -49,7 +91,7 @@ public class TestController {
      * @param paramMessageVOList
      * @return
      */
-    @RequestMapping(path = "request/bodyWithObject")
+    @RequestMapping(path = "bodyWithObject")
     public ResponseEntity<String> bodyWithObject(
             @RequestBody(required = false) List<ParamMessageVO> paramMessageVOList) {
         String responseString = "bodyJson=" + (paramMessageVOList!=null?paramMessageVOList.toString():null);
