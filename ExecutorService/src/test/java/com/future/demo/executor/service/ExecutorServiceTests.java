2ddc2a496fdbff477924ab9b45ebda9af7cfe0c7
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-03-01 13:47:15 +0800

    ExecutorService 新增ScheduledExecutorService演示

--- ExecutorService/src/test/java/com/future/demo/executor/service/ExecutorServiceTests.java
+++ ExecutorService/src/test/java/com/future/demo/executor/service/ExecutorServiceTests.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -44,5 +45,44 @@ public class ExecutorServiceTests {
 
         executorService.shutdown();
         while(!executorService.awaitTermination(500, TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void testSingleThreadScheduledExecutor() throws InterruptedException {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        for(int i=0; i<100; i++) {
+            int finalI = i;
+            executorService.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    logger.debug("Worker-" + finalI);
+                }
+            }, 5, 5, TimeUnit.SECONDS);
+        }
+
+        System.out.println();
+
+        executorService.shutdown();
+        while(!executorService.awaitTermination(100, TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(6);
+        for(int i=0; i<100; i++) {
+            int finalI = i;
+            executorService.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    Thread currentThread = Thread.currentThread();
+                    logger.debug("Current Thread {} execute Worker-{}", currentThread, finalI);
+                }
+            }, 5, 5, TimeUnit.SECONDS);
+        }
+
+        System.out.println();
+
+        executorService.shutdown();
+        while(!executorService.awaitTermination(100, TimeUnit.MILLISECONDS));
     }
 }
