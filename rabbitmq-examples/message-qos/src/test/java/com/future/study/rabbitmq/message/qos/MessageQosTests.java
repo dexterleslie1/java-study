231eae8f0a4e62a45e7ebf4c5a19720ab4c42ff7
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-05-03 07:37:28 +0800

    rabbitmq-examples/message-qos demo改进

--- rabbitmq-examples/message-qos/src/test/java/com/future/study/rabbitmq/message/qos/MessageQosTests.java
+++ rabbitmq-examples/message-qos/src/test/java/com/future/study/rabbitmq/message/qos/MessageQosTests.java
@@ -2,6 +2,7 @@ package com.future.study.rabbitmq.message.qos;
 
 import com.rabbitmq.client.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 
@@ -17,6 +18,21 @@ import java.util.concurrent.atomic.AtomicInteger;
  *
  */
 public class MessageQosTests {
+    String host;
+    String username;
+    String password;
+
+    @Before
+    public void setup() {
+        String host = System.getenv("host");
+        String username = System.getenv("username");
+        String password = System.getenv("password");
+
+        this.host = host;
+        this.username = username;
+        this.password = password;
+    }
+
     /**
      * 程序在没有消费者订阅队列前一次发送100个消息到消息队列，
      * 发送完毕后注册两个消费者qos分别为30%和70%比例分配100个消息到两个消费中
@@ -29,9 +45,9 @@ public class MessageQosTests {
         String queueName = UUID.randomUUID().toString();
 
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost(Config.RabbitMQServerHost);
-        connectionFactory.setUsername(Config.RabbitMQUsername);
-        connectionFactory.setPassword(Config.RabbitMQPassword);
+        connectionFactory.setHost(host);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
 
         Connection connection = connectionFactory.newConnection();
         Connection connectionWorker1 = connectionFactory.newConnection();
@@ -43,6 +59,8 @@ public class MessageQosTests {
         int countDownWorker2 = (int)(total*(1-percentageWorker1));
         AtomicInteger atomicIntegerWorker1 = new AtomicInteger();
         AtomicInteger atomicIntegerWorker2 = new AtomicInteger();
+        List<Delivery> deliveryList1 = new ArrayList<>();
+        List<Delivery> deliveryList2 = new ArrayList<>();
 
         Channel channel = connection.createChannel();
         channel.queueDeclare(queueName, false, false, false, null);
@@ -57,7 +75,7 @@ public class MessageQosTests {
             @Override
             public void handle(String consumerTag, Delivery delivery) throws IOException {
                 atomicIntegerWorker1.incrementAndGet();
-                channelWorker1.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                deliveryList1.add(delivery);
             }
         }, consumerTag -> {});
         Channel channelWorker2 = connectionWorker2.createChannel();
@@ -66,9 +84,16 @@ public class MessageQosTests {
             @Override
             public void handle(String consumerTag, Delivery delivery) throws IOException {
                 atomicIntegerWorker2.incrementAndGet();
-                channelWorker2.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                deliveryList2.add(delivery);
             }
         }, consumerTag -> {});
+
+        for(Delivery delivery : deliveryList1) {
+            channelWorker1.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        }
+        for(Delivery delivery : deliveryList2) {
+            channelWorker2.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        }
 
         int awaitTimeoutInSeconds = 1;
         Thread.sleep(awaitTimeoutInSeconds*1000);
@@ -77,6 +102,7 @@ public class MessageQosTests {
         connectionWorker1.close();
         connectionWorker2.close();
 
+        Assert.assertEquals(total, atomicIntegerWorker1.get()+atomicIntegerWorker2.get());
         Assert.assertEquals(countDownWorker1, atomicIntegerWorker1.get());
         Assert.assertEquals(countDownWorker2, atomicIntegerWorker2.get());
     }
