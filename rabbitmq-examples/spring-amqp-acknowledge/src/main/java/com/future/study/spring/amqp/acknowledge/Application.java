0a1ca17f731b2246c887ed53c7d68c05061494a9
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-05-03 09:09:05 +0800

    rabbitmq-examples/spring-amqp-acknowledge demo改进

--- rabbitmq-examples/spring-amqp-acknowledge/src/main/java/com/future/study/spring/amqp/acknowledge/Application.java
+++ rabbitmq-examples/spring-amqp-acknowledge/src/main/java/com/future/study/spring/amqp/acknowledge/Application.java
@@ -7,12 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Dexterleslie.Chan
  */
 @SpringBootApplication
-public class ApplicationSpringAMQPAcknowledge {
+public class Application {
     /**
      *
      * @param args
      */
     public static void main(String []args){
-        SpringApplication.run(ApplicationSpringAMQPAcknowledge.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
