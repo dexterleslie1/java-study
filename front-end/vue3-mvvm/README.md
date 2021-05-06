ece6cdf3dd7991794102052b0075a2c072fd2e37
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-05-06 20:21:20 +0800

    新增front-end/vue3-mvvm demo

---
+++ front-end/vue3-mvvm/README.md
@@ -0,0 +1,2 @@
+运行开发模式
+npm run serve
---
+++ front-end/vue3-mvvm/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  num: {{num}}
+  <br>
+  <input type="button" value="-" @click="sub()"/>
+  <input type="text" size="5" v-model="num"/>
+  <input type="button" value="+" @click="add()"/>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      num: 0,
+      min: 1,
+      max: 10
+    }
+  },
+  methods: {
+    sub() {
+      if(this.num <= this.min) {
+        this.num = this.min;
+      } else {
+        this.num--;
+      }
+    },
+    add() {
+      if(this.num >= this.max) {
+        this.num = this.max;
+      } else {
+        this.num++;
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
