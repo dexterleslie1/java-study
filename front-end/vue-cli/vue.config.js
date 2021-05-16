a389dc60e8512d9b839adda0a9f82f7963786cc1
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-05-16 20:35:16 +0800

    front-end/vue-cli 使用vue.config.js配置端口

--- front-end/vue-cli/vue.config.js
+++ front-end/vue-cli/vue.config.js
@@ -1,4 +1,10 @@
 // vue配置文件
 module.exports = {
-  outputDir: "build"
+  outputDir: "build",
+  // 配置dev server
+  devServer: {
+    // 配置端口
+    // https://blog.csdn.net/qq_39313596/article/details/83544745
+    port: 12345
+  }
 }
