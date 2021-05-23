d169109299382f10022ee14336f83195df50946a
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-05-23 13:37:38 +0800

    修改spring-cloud/spring-cloud-config-center demo

--- spring-cloud/spring-cloud-config-center/README.md
+++ spring-cloud/spring-cloud-config-center/README.md
@@ -1,3 +1,13 @@
 演示spring-cloud配置中心用法
 
 项目config-center-repository为demo配置中心配置文件仓库
+
+参考
+https://www.cnblogs.com/fengzheng/p/11242128.html
+
+调试config server是否正常
+获取main分支application.properties文件
+GET http://localhost:8888/application/default/main
+
+获取main分支application-dev.properties文件
+GET http://localhost:8888/application/dev/main
--- spring-cloud/spring-cloud-config-center/config-center-client/src/main/java/com/future/demo/config/center/client/controller/ApiController.java
+++ spring-cloud/spring-cloud-config-center/config-center-client/src/main/java/com/future/demo/config/center/client/controller/ApiController.java
@@ -1,15 +1,18 @@
 package com.future.demo.config.center.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
-    @Value("${hello}")
-    String hello;
-    @RequestMapping(value = "/hello")
-    public String hello(){
-        return "你好，" + hello;
+    @Value("${customize.args.int}")
+    private int customizeInt;
+    @Value("${customize.args.string}")
+    private String customizeString;
+
+    @GetMapping(value = "/getCustomizeArgs")
+    public String getCustomizeArgs(){
+        return "配置参数customize.args.int=" + customizeInt + ",customizeString=" + customizeString;
     }
 }
